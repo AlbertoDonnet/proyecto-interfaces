74a73a0bbccea0c22e40115b1c5c5f96e5bc475c
Alberto Donnet <donnetalberto@hotmail.com>
2025-06-11 03:09:37 -0300

    fallback a null

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
     adapter: adapter({
       pages: 'docs',
       assets: 'docs',
-      fallback: 'index.html',
+      fallback: null,
       precompress: false
     }),
     paths: {
